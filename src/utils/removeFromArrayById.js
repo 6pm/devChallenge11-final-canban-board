877ca1a95ea159e6c46ebf0f2b58ee241c6ad000
Slavik <slavik.troyan@gmail.com>
2017-07-01 10:52:32 +0300

    add doc

--- src/utils/removeFromArrayById.js
+++ src/utils/removeFromArrayById.js
@@ -1,4 +1,9 @@
-
+/**
+	 * Видалити із массива і вернути новий массив без видаленого елемента.
+	 * 
+	 * @param {Array} arr - массив з якого потрібно видалити
+	 * @param {String} id - id елемента який потрібно видалити
+	 */
 export default function removeFromArrayById(arr, id) {
 	const index = arr.map((i) => i.id).indexOf(id)
 	
